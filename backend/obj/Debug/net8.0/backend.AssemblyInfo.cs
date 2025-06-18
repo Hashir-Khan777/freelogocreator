10b9d7a88ea59d38e3f08d650613a76e3ac6a21d
Hashir-Khan777 <khnhashir555@gmail.com>
2025-06-18 22:46:00 +0000

    server

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd3a0fc8d128ea1437d996a14c60abcb125c80a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c148c9ad0d000b806e4732328a4c184985d33d7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
